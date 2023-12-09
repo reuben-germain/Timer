432602e5b56862e26a269535631c2e50fcd7b582
Reuben Germain <reuben.germain@soprasteria.com>
2023-12-09 15:13:07 +0000

    took whatever minutes there are (25) & converted it to seconds

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Data & Variables
 const SECONDS_IN_MINUTES = 60;
+let timeLeft = 25 * SECONDS_IN_MINUTES + 0; //MINUTES AS SECONDS PLUS THE SECONDS
 //Cache the DOM
 //Add Listeners
 //Intialization
